e4a638b6620b370d96755578773d7e02aaa55d66
mancharlie45 <ubahcharl45@gmail.com>
2022-09-01 01:03:33 +0100

    Initial commit

---
+++ Play Fetch/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+     private float timer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && timer > 0.3)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                timer = 0;
+        }
+    
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Play-Fetch
+ This is exercise2
